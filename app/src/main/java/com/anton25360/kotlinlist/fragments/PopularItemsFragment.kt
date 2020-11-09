a04bd85c28ae80afb91f6149e2e18a8edd1487d9
Anton Webb <anton25360@gmail.com>
2020-11-09 13:02:42 +0000

    added gson

--- app/src/main/java/com/anton25360/kotlinlist/fragments/PopularItemsFragment.kt
+++ app/src/main/java/com/anton25360/kotlinlist/fragments/PopularItemsFragment.kt
@@ -1,21 +1,19 @@
 package com.anton25360.kotlinlist.fragments
 
 import android.content.ContentValues.TAG
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.BaseAdapter
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.anton25360.kotlinlist.R
 import kotlinx.android.synthetic.main.fragment_popular_items.*
 import okhttp3.*
+import org.json.JSONObject
 import java.io.IOException
-import java.net.URL
+
 
 class PopularItemsFragment : Fragment() {
 
@@ -25,8 +23,8 @@ class PopularItemsFragment : Fragment() {
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_popular_items, container, false)
@@ -34,7 +32,7 @@ class PopularItemsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val words = arrayOf("one", "two", "three")
-        piList.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1,words)
+        piList.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, words)
 
 
         fetchJSon()
@@ -62,11 +60,13 @@ class PopularItemsFragment : Fragment() {
 
             override fun onResponse(call: Call, response: Response) {
                 val body = response.body?.string()
+//                val jsonObject = JSONObject(body)
                 println(body)
+
             }
 
             override fun onFailure(call: Call, e: IOException) {
-                println("failed to execute request: "+e)
+                println("failed to execute request: " + e)
             }
         })
     }
