85464d51966ad385bb99245b2e33a0dac750c960
Anton Webb <anton25360@gmail.com>
2020-11-10 13:12:11 +0000

    title and quantity output to logs

--- app/src/main/java/com/anton25360/kotlinlist/PopularItemDetailActivity.kt
+++ app/src/main/java/com/anton25360/kotlinlist/PopularItemDetailActivity.kt
@@ -3,17 +3,27 @@ package com.anton25360.kotlinlist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_popular_item_detail.*
 
 class PopularItemDetailActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_popular_item_detail)
 
-        //get the clicked item
-        val dataFromIntent = intent.getStringArrayListExtra("chosenItem")
-        val item = dataFromIntent?.get(0)
+        val dataFromIntent = intent.getStringArrayListExtra("chosenItem") //data from intent
+        val item = dataFromIntent?.get(0) //get the name of the clicked item
+        popular_item_detail_textView.text = "$item" //set item text
+    }
 
-        popular_item_detail_textView.text = "$item" //display clicked item
+
+    fun toastMe(view: View) {
+        val dataFromIntent = intent.getStringArrayListExtra("chosenItem") //data from intent
+        val item = dataFromIntent?.get(0) //get the name of the clicked item
+        val quantity = popular_item_detail_inputField.text
+
+        Toast.makeText(this, "Add $item x $quantity", Toast.LENGTH_SHORT).show()
     }
 }
