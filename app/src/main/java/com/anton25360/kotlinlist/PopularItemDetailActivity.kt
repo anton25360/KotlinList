492e7db887d4398ac3202e35bfa31a6dd1f8efe3
Anton Webb <anton25360@gmail.com>
2020-11-10 13:29:56 +0000

    Added validation to input field

--- app/src/main/java/com/anton25360/kotlinlist/PopularItemDetailActivity.kt
+++ app/src/main/java/com/anton25360/kotlinlist/PopularItemDetailActivity.kt
@@ -2,7 +2,6 @@ package com.anton25360.kotlinlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_popular_item_detail.*
@@ -16,14 +15,20 @@ class PopularItemDetailActivity : AppCompatActivity() {
         val dataFromIntent = intent.getStringArrayListExtra("chosenItem") //data from intent
         val item = dataFromIntent?.get(0) //get the name of the clicked item
         popular_item_detail_textView.text = "$item" //set item text
+
+//        popular_item_detail_button.setOnClickListener {toastMe()} //run function on btn click
     }
 
 
     fun toastMe(view: View) {
         val dataFromIntent = intent.getStringArrayListExtra("chosenItem") //data from intent
         val item = dataFromIntent?.get(0) //get the name of the clicked item
-        val quantity = popular_item_detail_inputField.text
+        val quantity = popular_item_detail_inputField.text.toString()
 
-        Toast.makeText(this, "Add $item x $quantity", Toast.LENGTH_SHORT).show()
+        if (popular_item_detail_inputField.length() == 0 || quantity == "0"){
+            popular_item_detail_inputField.setError("Must not be empty")
+        } else {
+            Toast.makeText(this, "Add $item x $quantity", Toast.LENGTH_SHORT).show()
+        }
     }
 }
